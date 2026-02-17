a68facdd3aaff93109e7ffc1223181a41b5e39d8
Rutvija Mali <rutvijamali@gmail.com>
2026-02-17 23:18:35 +0530

    debug

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -58,7 +58,7 @@ export default function Dashboard() {
       "postgres_changes",
       { event: "INSERT", schema: "public", table: "bookmarks" },
       () => {
-        fetchBookmarks();
+        setBookmarks((prevBookmarks) => [...prevBookmarks, payload]);
       }
     )
     .on(
